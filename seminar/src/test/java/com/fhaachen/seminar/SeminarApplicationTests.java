142768b9e0449ef7f4facb61d9a52b2a7c6892a9
eg1031s <erik.grobel@alumni.fh-aachen.de>
2024-01-05 15:40:13 +0100

    Initinal Commit

---
+++ seminar/src/test/java/com/fhaachen/seminar/SeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhaachen.seminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
